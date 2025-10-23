410d55de5ad4aaccc41d81596c0d0189aec3116a
Aurora Gaffney <aurora@blinklabs.io>
2025-10-23 13:35:50 -0400

    chore: update EKS version to 1.34 (#49)
    Signed-off-by: Aurora Gaffney <aurora@blinklabs.io>

--- terraform/aws-eks.tf
+++ terraform/aws-eks.tf
@@ -3,7 +3,7 @@ module "eks" {
 
   for_each        = { for c in try(local.env_vars.aws.clusters, {}) : c.name => c }
   cluster_name    = each.value.name
-  cluster_version = try(each.value.cluster_version, "1.32")
+  cluster_version = try(each.value.cluster_version, "1.34")
   cidr            = try(each.value.cidr, "10.10.0.0/16")
   azs             = try(each.value.azs, ["us-east-1a", "us-east-1b", "us-east-1d"])
   public_subnets  = try(each.value.public_subnets, ["10.10.0.0/20", "10.10.16.0/20", "10.10.32.0/20"])
