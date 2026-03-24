6a0e24da95bddf9b752dc3b6d605d366fd265a75
Tripura Repalle <tripura@blinklabs.io>
2026-03-24 09:20:20 -0400

    feat(add-iam-irsa-role): added iam irsa role
    Signed-off-by: Tripura Repalle <tripura@blinklabs.io>

--- terraform/aws-iam.tf
+++ terraform/aws-iam.tf
@@ -5,6 +5,65 @@ module "aws_iam" {
   policies = try(local.env_vars.aws.iam.policies, [])
 }
 
+data "aws_eks_cluster" "this" {
+  for_each = { for c in try(local.env_vars.aws.clusters, {}) : c.name => c }
+  name     = each.value.name
+
+  depends_on = [module.eks]
+}
+
+data "aws_iam_openid_connect_provider" "eks" {
+  for_each = { for c in try(local.env_vars.aws.clusters, {}) : c.name => c }
+  url      = data.aws_eks_cluster.this[each.key].identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_policy" "external_dns" {
+  name        = "external-dns-route53-access"
+  description = "Allows external-dns to manage Route53 records in b7s.services zone"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "route53:ChangeResourceRecordSets",
+        ]
+        Resource = "arn:aws:route53:::hostedzone/*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "route53:ListHostedZones",
+          "route53:ListResourceRecordSets",
+          "route53:ListTagsForResource",
+        ]
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+module "external_dns_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  for_each = { for c in try(local.env_vars.aws.clusters, {}) : c.name => c }
+
+  role_name = "${each.value.name}-external-dns"
+
+  oidc_providers = {
+    main = {
+      provider_arn               = data.aws_iam_openid_connect_provider.eks[each.key].arn
+      namespace_service_accounts = ["kube-system:external-dns"]
+    }
+  }
+
+  role_policy_arns = {
+    external_dns = aws_iam_policy.external_dns.arn
+  }
+}
+
 # Automatically import IAM user access keys by ID
 # This can't be in the module, so we put it here
 import {
